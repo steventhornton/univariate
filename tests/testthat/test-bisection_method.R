7f09f05757c838a699e23372d7c557ce1027a107
steventhornton <steven@rnfc.org>
2020-06-03 16:32:23 -0400

    Added more tests for bisection method

--- tests/testthat/test-bisection_method.R
+++ tests/testthat/test-bisection_method.R
@@ -4,3 +4,14 @@ test_that("error for same sign", {
   f <- function(x) x
   expect_error(bisection_method(f, x0, x1))
 })
+
+test_that("error for wrong endpoints", {
+  x0 <- 1
+  x1 <- -1
+  f <- function(x) x
+  expect_error(bisection_method(f, x0, x1))
+})
+
+test_that("sample rootfinding", {
+  expect_equal(bisection_method(sin, -pi, pi), 0)
+})
